cddb41b1281abb75e822f6eb100305827cd1744b
Tom <tommusique974@gmail.com>
2022-01-28 18:49:15 +0100

    en cours

--- index.php
+++ index.php
@@ -15,8 +15,10 @@ require_once('./fonction/Selection.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projet avion</title>
+    <!-- Css Perso -->
     <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="css/TopBar.css">
+    <link rel="stylesheet" href="css/RightBar.css">
     <!-- CDN Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -25,7 +27,25 @@ require_once('./fonction/Selection.php');
 </head>
 
 <body>
-    <h1>Hello World</h1>
+    <!-- Bar du haut -->
+    <div class="Topbar">
+        <img src="assets/logo.png" alt="logo">
+        <div class="leftSide">
+            <div class="PP"></div>
+            <p>Admin</p>
+        </div>
+    </div>
+    <!-- Right Bar menu -->
+    <div class="RightBar">
+        <h4>Accueil</h4>
+        <h4>Formulaire</h4>
+        <div class="block">
+            <p>Vols</p>
+            <p>Avions</p>
+            <p>Pilotes</p>
+            <p>Aéroport</p>
+        </div>
+    </div>  
     <!-- CDN Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
