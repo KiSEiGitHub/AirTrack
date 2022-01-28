078da172935514ee86709d7549fa5e4ce006455f
Tom <tommusique974@gmail.com>
2022-01-28 13:38:06 +0100

    feat(*) : select, delete

--- fonction/Selection.php
+++ fonction/Selection.php
@@ -1,6 +1,15 @@
 <!-- Fonction de selection  -->
 <?php
-function Selection()
+function Selection($table)
 {
     $con = connexion();
+    $r = "SELECT * FROM $table";
+
+    if ($con) {
+        $Data = mysqli_query($con, $r);
+    } else {
+        return null;
+    }
+    deconnexion($con);
+    return $Data;
 }
