001a1acdedb63260596d45e6062c893d5ea75530
Tom <tommusique974@gmail.com>
2022-01-28 19:07:31 +0100

    attente du style du texte

--- index.php
+++ index.php
@@ -40,12 +40,35 @@ require_once('./fonction/Selection.php');
         <h4>Accueil</h4>
         <h4>Formulaire</h4>
         <div class="block">
-            <p>Vols</p>
-            <p>Avions</p>
-            <p>Pilotes</p>
-            <p>Aéroport</p>
+            <p>
+                <i class="fas fa-plane-departure iconSpacing"></i> Vols
+            </p>
+            <p>
+                <i class="fas fa-plane iconSpacing"></i> Avions
+            </p>
+            <p>
+                <i class="fas fa-user-tie iconSpacing"></i> Pilotes
+            </p>
+            <p>
+                <i class="fas fa-map-marker-alt iconSpacing"></i> Aéroport
+            </p>
         </div>
-    </div>  
+        <h4>Informations</h4>
+        <div class="block">
+            <p>
+                <i class="fas fa-plane-departure iconSpacing"></i> Vols
+            </p>
+            <p>
+                <i class="fas fa-plane iconSpacing"></i> Avions
+            </p>
+            <p>
+                <i class="fas fa-user-tie iconSpacing"></i> Pilotes
+            </p>
+            <p>
+                <i class="fas fa-map-marker-alt iconSpacing"></i> Aéroport
+            </p>
+        </div>
+    </div>
     <!-- CDN Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
