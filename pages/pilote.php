38f57102648dfced5b808a42687c6fb6fe79d032
Tom <tommusique974@gmail.com>
2022-01-29 13:18:20 +0100

    feat(*) : base setup ok

--- pages/pilote.php
+++ pages/pilote.php
@@ -1,1 +1,9 @@
 <!-- Ici sera la page ou on affichera le tableau des pilotes de la bdd -->
+<h1 class="font-regular-blue">Information / Pilote</h1>
+
+<!-- Tout votre code doit être dans la div FrontBlock -->
+<!-- Pour chaque page crée vous un css et ajouter à la suite dans index.php -->
+<div class="FrontBlock">
+    <!-- Coder en dessous -->
+    <h1>Hello World</h1>
+</div>
--- vues/aeroport.php
+++ vues/aeroport.php
@@ -1,1 +1,9 @@
 <!-- Ici sera la page ou on affichera le formulaire pour insérer des aéroport dans la bdd -->
+<h1 class="font-regular-blue">Formulaire / Aéroport</h1>
+
+<!-- Tout votre code doit être dans la div FrontBlock -->
+<!-- Pour chaque page crée vous un css et ajouter à la suite dans index.php -->
+<div class="FrontBlock">
+    <!-- Coder en dessous -->
+    <h1>Hello World</h1>
+</div>
