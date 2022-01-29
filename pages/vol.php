7b679c5985163a5925301585ceeb973e94541d57
Tom <tommusique974@gmail.com>
2022-01-29 21:49:14 +0100

    feat(*) : information / avions

--- pages/vol.php
+++ pages/vol.php
@@ -1,10 +1,35 @@
 <!-- Ici sera la page ou on affichera le tableau des vol de la bdd -->
 <h1 class="font-regular-blue">Information / Vols</h1>
 
-
 <!-- Tout votre code doit être dans la div FrontBlock -->
 <!-- Pour chaque page crée vous un css et ajouter à la suite dans index.php -->
 <div class="FrontBlock">
     <!-- Coder en dessous -->
-    <h1>Hello World</h1>
+    <div class="IconCenter">
+        <i class="fas fa-plane-departure"></i>
+    </div>
+
+    <table class="GlobalTable">
+        <th>N° de vol</th>
+        <th>Durée</th>
+        <th>Date</th>
+        <th>Matricule de l'avion'</th>
+        <th>Aérport de départ</th>
+        <th>Aéroport d'arrivé</th>
+        <th>Pilote n°1</th>
+        <th>Pilote n°2</th>
+        <th>Opération</th>
+        <tr>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+            <td>exemple</td>
+        </tr>
+    </table>
+    
 </div>
