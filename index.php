1b4859f6bc5693bfa5c6c5be16053c6b662d7f7f
Tom <tommusique974@gmail.com>
2022-01-29 21:04:19 +0100

    fix(index) : j'avais inverser les links

--- index.php
+++ index.php
@@ -63,27 +63,6 @@ require_once('./fonction/LinkFunction.php');
             <div>
                 <div class="sous-block">
                     <i class="fas fa-plane-departure iconSpacing"></i>
-                    <a href="index.php?page=2">Vols</a>
-                </div>
-                <div class="sous-block">
-                    <i class="fas fa-plane iconSpacing"></i>
-                    <a href="index.php?page=3" style="position: relative; left: 2px">Avions</a>
-                </div>
-                <div class="sous-block">
-                    <i class="fas fa-user-tie iconSpacing"></i>
-                    <a href="index.php?page=4" style="position: relative; left: 7px">Pilotes</a>
-                </div>
-                <div class="sous-block">
-                    <i class="fas fa-map-marker-alt iconSpacing"></i>
-                    <a href="index.php?page=5" style="position: relative; left: 8px">Aéroport</a>
-                </div>
-            </div>
-        </div>
-        <h4 class="font-regular-white">Informations</h4>
-        <div class="block">
-            <div>
-                <div class="sous-block">
-                    <i class="fas fa-plane-departure iconSpacing"></i>
                     <a href="index.php?page=6">Vols</a>
                 </div>
                 <div class="sous-block">
@@ -97,6 +76,27 @@ require_once('./fonction/LinkFunction.php');
                 <div class="sous-block">
                     <i class="fas fa-map-marker-alt iconSpacing"></i>
                     <a href="index.php?page=9" style="position: relative; left: 8px">Aéroport</a>
+                </div>
+            </div>
+        </div>
+        <h4 class="font-regular-white">Informations</h4>
+        <div class="block">
+            <div>
+                <div class="sous-block">
+                    <i class="fas fa-plane-departure iconSpacing"></i>
+                    <a href="index.php?page=2">Vols</a>
+                </div>
+                <div class="sous-block">
+                    <i class="fas fa-plane iconSpacing"></i>
+                    <a href="index.php?page=3" style="position: relative; left: 2px">Avions</a>
+                </div>
+                <div class="sous-block">
+                    <i class="fas fa-user-tie iconSpacing"></i>
+                    <a href="index.php?page=4" style="position: relative; left: 7px">Pilotes</a>
+                </div>
+                <div class="sous-block">
+                    <i class="fas fa-map-marker-alt iconSpacing"></i>
+                    <a href="index.php?page=5" style="position: relative; left: 8px">Aéroport</a>
                 </div>
             </div>
         </div>
