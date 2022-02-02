43631f8ee3da3da374dada47c801e68203a7e6f4
Tom <tommusique974@gmail.com>
2022-02-02 19:04:09 +0100

    fix(*) : id for labels

--- vues/vol.php
+++ vues/vol.php
@@ -17,46 +17,47 @@
             <div class = "parent">
                 <div>
                     <label for = "Numvol" class = "font-regular-blue-16">Numéro de vol</label>
-                    <input type = "text" name = "numvol" placeholder = "Numéro de vol">
+                    <input type = "text" name = "numvol" placeholder = "Numéro de vol" id="Numvol">
                 </div>
                 <div>
                     <label for = "durvol" class = "font-regular-blue-16">Durée du vol (en minutes)</label>
-                    <input type = "text" name = "durvol" placeholder = "Durée de vol">
+                    <input type = "text" name = "durvol" placeholder = "Durée de vol" id="durvol">
                 </div>
             </div>
             <div class = "parent">
                 <div>
                     <label for = "datevol" class = "font-regular-blue-16">Date du vol</label>
-                    <input type = "date" name = "datevol" placeholder = "Date en val (jj/mm/aa)">
+                    <input type = "date" name = "datevol" placeholder = "Date en val (jj/mm/aa)" id="datevol">
                 </div>
                 <div>
                     <!--  Ici c'est un select des avions -->
                     <label for = "matriculeavion" class = "font-regular-blue-16">Matricule de l'avion</label>
-                    <input type = "number" name = "matriculeavion" placeholder = "Matricule de l'avion">
+                    <input type = "number" name = "matriculeavion" placeholder = "Matricule de l'avion"
+                           id = "matriculeavion">
                 </div>
             </div>
             <div class = "parent">
                 <div>
                     <!--  Ici c'est un select des aéroport -->
                     <label for = "aerodepart" class = "font-regular-blue-16">Aéroport de départ</label>
-                    <input type = "number" name = "aerodepart" placeholder = "Aéroport de départ">
+                    <input type = "number" name = "aerodepart" placeholder = "Aéroport de départ" id = "aerodepart">
                 </div>
                 <div>
                     <label for = "aeroarrive" class = "font-regular-blue-16">Aéroport d'arrivé</label>
-                    <input type = "number" name = "aeroarrive" placeholder = "Aéroport d'arrivé">
+                    <input type = "number" name = "aeroarrive" placeholder = "Aéroport d'arrivé" id = "aeroarrive">
                 </div>
             </div>
             <div class = "parent">
                 <div>
                     <!--  Ici c'est un select des pilotes -->
                     <label for = "pilone" class = "font-regular-blue-16">Pilote n°1</label>
-                    <select name = "pilone">
+                    <select name = "pilone" id="pilone">
                         <option value = "">Choisissez un pilote</option>
                         <?php
                         $LesPilotes = Selection("pilote");
                         foreach ($LesPilotes as $Pilote) {
                             echo "<option value='" . $Pilote['idpilote'] . "'>";
-                            echo $Pilote['prenom']. " " . $Pilote['nom'];
+                            echo $Pilote['prenom'] . " " . $Pilote['nom'];
                             echo "</option>";
                         }
                         ?>
@@ -64,13 +65,13 @@
                 </div>
                 <div>
                     <label for = "piltwo" class = "font-regular-blue-16">Pilote n°2</label>
-                    <select name = "piltwo">
+                    <select name = "piltwo" id="piltwo">
                         <option value = "">Choisissez un pilote</option>
                         <?php
                         $LesPilotes = Selection("pilote");
                         foreach ($LesPilotes as $Pilote) {
                             echo "<option value='" . $Pilote['idpilote'] . "'>";
-                            echo $Pilote['prenom']. " " . $Pilote['nom'];
+                            echo $Pilote['prenom'] . " " . $Pilote['nom'];
                             echo "</option>";
                         }
                         ?>
@@ -116,7 +117,7 @@ if (isset($_POST['btn-sub'])) {
                 $_POST['piltwo'],
                 8
             );
-        };
+        }
 
     } else {
         echo "<p class='text-danger'>Il faut remplir tous les champs</p>";
