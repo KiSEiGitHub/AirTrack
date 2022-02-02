5fe03bc6c2bf530ff68f35ea8df24b61f00c0f02
Tom <tommusique974@gmail.com>
2022-02-02 18:57:29 +0100

    format

--- pages/Home.php
+++ pages/Home.php
@@ -1,38 +1,38 @@
-<h1 class="font-regular-blue">Tableau de bord</h1>
-
+<h1 class = "font-regular-blue">Tableau de bord</h1>
 <!-- Tout votre code doit être dans la div FrontBlock -->
 <!-- Pour chaque page crée vous un css et ajouter à la suite dans index.php -->
-<div class="FrontBlock">
+<div class = "FrontBlock">
     <!-- BLock Projet -->
-    <div class="projetBlock">
-        <div class="Projettitle">
-            <h4 class="font-regular-blue">Projet</h4>
-        </div>
-        <div class="ProjetPara">
 
-            <p class="font-regular-blue-16">
+    <div class = "projetBlock">
+        <div class = "Projettitle">
+            <h4 class = "font-regular-blue">Projet</h4>
+        </div>
+        <div class = "ProjetPara">
+
+            <p class = "font-regular-blue-16">
                 Nous sommes un groupe de 3 élèves de BTS SIO et avons pour projet de vous présenter un site internet
                 parmi plusieurs thèmes proposés. Nous avons choisi la gestion des vols d'Air France, avec le détail des
                 avions, pilotes et aéroports.
             </p>
 
-            <p class="font-regular-blue-16">
+            <p class = "font-regular-blue-16">
                 Le cahier des charges nous imposait les contraintes suivantes : <br>
                 - Possibilité de connexion / déconnexion avec des rôles <br>
                 - Ajout d'actions usuelles sur les tables (ajouter, modifier, supprimer, lister) <br>
                 - Afficher une barre de menu ainsi qu'un pied de page
             </p>
 
-            <p class="font-regular-blue-16">
+            <p class = "font-regular-blue-16">
                 Nous sommes heureux de vous présenter notre site et espérons qu'il ai atteint nos objectifs.
             </p>
         </div>
     </div>
 
     <!-- BLock Entrées -->
-    <div class="projetBlock">
-        <div class="Projettitle">
-            <h4 class="font-regular-blue">Entrées</h4>
+    <div class = "projetBlock">
+        <div class = "Projettitle">
+            <h4 class = "font-regular-blue">Entrées</h4>
         </div>
         <?php
         $LesVol = CountAll("vol");
@@ -40,12 +40,12 @@
         $LesPilotes = CountAll("pilote");
         $LesAero = CountAll("aeroport");
         ?>
-        <table class="homeTab">
+        <table class = "homeTab">
             <tr>
-                <th class="font-regular-blue-16">Vols</th>
-                <th class="font-regular-blue-16">Avions</th>
-                <th class="font-regular-blue-16">Pilotes</th>
-                <th class="font-regular-blue-16">Aéroport</th>
+                <th class = "font-regular-blue-16">Vols</th>
+                <th class = "font-regular-blue-16">Avions</th>
+                <th class = "font-regular-blue-16">Pilotes</th>
+                <th class = "font-regular-blue-16">Aéroport</th>
             </tr>
             <tr>
                 <?php
