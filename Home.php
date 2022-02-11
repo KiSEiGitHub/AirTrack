5d257c5f632a0a9268bc17e7aecabf3d380cab1c
Tom <tommusique974@gmail.com>
2022-02-11 22:23:52 +0100

    style(*) : refonte

--- Home.php
+++ Home.php
@@ -16,60 +16,58 @@ require_once('./fonction/Update.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset = "UTF-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <title>Projet avion</title>
 
     <!-- Css Perso -->
-    <link rel="stylesheet" href="css/TopBar.css">
-    <link rel="stylesheet" href="css/RightBar.css">
-    <link rel="stylesheet" href="css/MainBlock.css">
-    <link rel="stylesheet" href="css/Home.css">
-    <link rel="stylesheet" href="css/Connexion.css">
-    <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/taches.css">
+    <link rel = "stylesheet" href = "css/TopBar.css">
+    <link rel = "stylesheet" href = "css/RightBar.css">
+    <link rel = "stylesheet" href = "css/MainBlock.css">
+    <link rel = "stylesheet" href = "css/Home.css">
+    <link rel = "stylesheet" href = "css/Connexion.css">
+    <link rel = "stylesheet" href = "css/footer.css">
+    <link rel = "stylesheet" href = "css/taches.css">
 
     <!-- CDN Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous">
+    <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel = "stylesheet"
+          integrity = "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin = "anonymous">
 
     <!-- reset CSS + Global CSS -->
-    <link rel="stylesheet" href="style.css">
+    <link rel = "stylesheet" href = "style.css">
 
     <!-- CDN Font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
 
-<div class="Topbar">
-    <img src="assets/logo.png" alt="logo">
-    <div class="leftSide">
-        <div class="PP">
+<div class = "Topbar">
+    <img src = "assets/logo.png" alt = "logo">
+    <div class = "leftSide">
+        <div class = "PP">
             <?php
-                if($_SESSION['pseudo'] == 'Clara'){
-                    echo "<img src='./assets/img_Clara.jpg' alt='pp' />";
-                } else if ($_SESSION['pseudo'] == 'Salim'){
-                    echo "<img src='./assets/img_Salim.jpg' alt='pp' />";
-                } else if ($_SESSION['pseudo'] == 'Tom') {
-                    echo "<img src='./assets/img_Tom.jpg' alt='pp' />";
-                }
+            if ($_SESSION['pseudo'] == 'Clara') {
+                echo "<img src='./assets/img_Clara.jpg' alt='pp' />";
+            } else if ($_SESSION['pseudo'] == 'Salim') {
+                echo "<img src='./assets/img_Salim.jpg' alt='pp' />";
+            } else if ($_SESSION['pseudo'] == 'Tom') {
+                echo "<img src='./assets/img_Tom.jpg' alt='pp' />";
+            } else {
+                echo "<i class='fa-solid fa-user-check' style='font-size: 30px'></i>";
+            }
             ?>
         </div>
-        <span class="badge bg-primary" style="position:relative; left: 20px; top: -12px">
-            <?php
-            echo $_SESSION['pseudo']
-            ?>
+        <span class = "badge bg-primary" style = "position:relative; left: 20px; top: -12px">
+            <?php echo $_SESSION['pseudo'] ?>
         </span>
-        <span class="badge bg-danger" style="position:relative; left: -20px; top: 12px">
-            <?php
-                echo $_SESSION['rolee']
-            ?>
+        <span class = "badge bg-danger" style = "position:relative; left: -20px; top: 12px">
+           <?php echo $_SESSION['rolee'] ?>
         </span>
     </div>
 
@@ -80,8 +78,8 @@ require_once('./Components/Rightbar.php');
 
 ?>
 
-<div class="mainBlock">
-    <div class="Sous-Main-Block">
+<div class = "mainBlock">
+    <div class = "Sous-Main-Block">
         <?php
         if (isset($_GET['page'])) {
             LinkFunction($_GET['page']);
@@ -95,11 +93,11 @@ require_once('./Components/Rightbar.php');
 <?php require_once('./Components/Footer.php') ?>
 
 <!--  js  -->
-<script src="main.js"></script>
+<script src = "main.js"></script>
 <!-- CDN Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+<script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity = "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin = "anonymous"></script>
 </body>
 
 </html>
--- pages/Home.php
+++ pages/Home.php
@@ -1,56 +1,57 @@
-<h1 class="font-regular-blue">Tableau de bord</h1>
+<h1 class = "font-regular-blue">Tableau de bord</h1>
 <!-- Tout votre code doit être dans la div FrontBlock -->
 <!-- Pour chaque page crée vous un css et ajouter à la suite dans index.php -->
-<div class="FrontBlock">
+<div class = "FrontBlock">
     <!-- BLock Projet -->
 
-    <div class="projetBlock">
-        <div class="Projettitle">
-            <h4 class="font-regular-blue">Notre projet de groupe</h4>
+    <div class = "projetBlock">
+        <div class = "Projettitle">
+            <h4 class = "font-regular-blue">Notre projet de groupe</h4>
         </div>
 
-        <div class="BlockIcon">
-            <i class="fa-solid fa-users test"></i>
+        <div class = "BlockIcon">
+            <i class = "fa-solid fa-users test"></i>
         </div>
 
-        <div class="ProjetPara">
-            <p class="font-regular-blue-16">
+        <div class = "ProjetPara">
+            <p class = "font-regular-blue-16">
                 Nous sommes un groupe de 3 élèves de BTS SIO et avons pour projet de vous présenter un site internet
                 parmi plusieurs thèmes proposés. Nous avons choisi la gestion des vols d'Air France, avec le détail des
                 avions, pilotes et aéroports.
             </p>
 
-            <p class="font-regular-blue-16">
+            <p class = "font-regular-blue-16">
                 Le cahier des charges nous imposait les contraintes suivantes : <br>
                 - Possibilité de connexion / déconnexion avec des rôles <br>
                 - Ajout d'actions usuelles sur les tables (ajouter, modifier, supprimer, lister) <br>
                 - Afficher une barre de menu ainsi qu'un pied de page
             </p>
 
-            <p class="font-regular-blue-16">
+            <p class = "font-regular-blue-16">
                 Nous sommes heureux de vous présenter notre site et espérons qu'il ai atteint nos objectifs.
             </p>
         </div>
     </div>
 
-    <div class="Ecole">
-         <div class="IconCenter">
-            <i class="fa-solid fa-graduation-cap"></i>
-         </div>
-        <div class="cfainsta">
-         <p class="font-regular-blue">Le CFA INSTA</p>
-         </div>
+    <div class = "Ecole">
+        <div class = "IconCenter">
+            <i class = "fa-solid fa-graduation-cap"></i>
+        </div>
+        <div class = "cfainsta">
+            <p class = "font-regular-blue">Le CFA INSTA</p>
+        </div>
 
-         <div class="cfalien">
-             <p>Retrouvez le site de notre école juste <a class="cfaliencolor" href="https://cfa-insta.fr/">ici</a></p>
-         </div>
+        <div class = "cfalien">
+            <p>Retrouvez le site de notre école juste <a class = "cfaliencolor" href = "https://cfa-insta.fr/"
+                                                         target = "_blank">ici</a></p>
+        </div>
 
     </div>
 
     <!-- BLock Entrées -->
-    <div class="projetBlock">
-        <div class="Projettitle">
-            <h4 class="font-regular-blue">Entrées</h4>
+    <div class = "projetBlock">
+        <div class = "Projettitle">
+            <h4 class = "font-regular-blue">Entrées</h4>
         </div>
         <?php
         $LesVol = CountAll("vol");
@@ -58,12 +59,12 @@
         $LesPilotes = CountAll("pilote");
         $LesAero = CountAll("aeroport");
         ?>
-        <table class="homeTab">
+        <table class = "homeTab">
             <tr>
-                <th class="font-regular-blue-16">Vols</th>
-                <th class="font-regular-blue-16">Avions</th>
-                <th class="font-regular-blue-16">Pilotes</th>
-                <th class="font-regular-blue-16">Aéroport</th>
+                <th class = "font-regular-blue-16">Vols</th>
+                <th class = "font-regular-blue-16">Avions</th>
+                <th class = "font-regular-blue-16">Pilotes</th>
+                <th class = "font-regular-blue-16">Aéroport</th>
             </tr>
             <tr>
                 <?php
